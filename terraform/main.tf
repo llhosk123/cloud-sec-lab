d27e65e2edcd60cb9636d5f482c21b1d1b605e66
llhosk123 <llhosk123@users.noreply.github.com>
2026-03-13 16:14:54 +0900

    reset

---
+++ terraform/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["099720109477"]
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+resource "aws_instance" "lab" {
+
+  ami = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.lab.id
+  ]
+
+  tags = {
+    Name = "lab-instance"
+  }
+}
