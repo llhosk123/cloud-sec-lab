1bb4b15629305cca421843203f5250ca6b73cd32
llhosk123 <llhosk123@users.noreply.github.com>
2026-03-13 16:03:56 +0900

    auto deploy web

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,3 +6,14 @@ curl -sfL https://get.k3s.io | sh -s - \
   --disable traefik \
   --disable servicelb \
   --disable metrics-server
+
+sleep 20
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+
+sleep 30
+
+kubectl apply -f /home/ubuntu/cloud-sec-lab/k8s/web.yaml
+kubectl apply -f /home/ubuntu/cloud-sec-lab/k8s/ingress.yaml
