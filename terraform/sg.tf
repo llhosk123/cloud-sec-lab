b1c1c130dc96eafa8b7fefe45cf31744bea23ed0
llhosk123 <llhosk123@users.noreply.github.com>
2026-03-25 10:42:44 +0900

    보안그룹 전체 개방

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -1,33 +1,25 @@
-resource "aws_security_group" "main" {
+resource "aws_security_group" "ids_sg" {
+  name        = "ids-security-group"
+  description = "Allow all inbound traffic for IDS testing"
+  vpc_id      = aws_vpc.main.id # 본인의 VPC ID 변수명
 
-  name_prefix = "lab-sg-"
-
+  # 인바운드 규칙: 모든 포트, 모든 프로토콜 개방
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          # -1은 모든 프로토콜(TCP, UDP, ICMP 등)을 의미합니다.
+    cidr_blocks = ["0.0.0.0/0"] # 전 세계 어디서든 접속 허용
   }
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 30000
-    to_port = 32767
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # 아웃바운드 규칙: 기본적으로 모두 허용
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Name = "ids-sg-testing"
+  }
 }
